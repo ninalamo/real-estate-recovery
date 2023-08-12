dd5bbdce0145b53c235926b10ddd3030c89a6352
Ninism <nin.alamo@outlook.com>
2023-08-12 22:04:56 +0800

    iniital commit

--- Project1/Controllers/WeatherForecastController.cs
+++ Project1/Controllers/WeatherForecastController.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Project1.Controllers
 {
-    [Authorize]
+    // [Authorize]
     [ApiController]
     [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
